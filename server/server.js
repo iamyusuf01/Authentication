d85745d288ed92bb0973bf144a245093df5a21a7
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-04-21 10:39:47 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ connectDB();
 
 const allowedOrigins = ["http://localhost:5173"]
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 
